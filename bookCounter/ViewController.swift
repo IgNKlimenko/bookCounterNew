7e64980c4d140f776f9c86e87bcd95c947155691
IgNKlimenko <Ksua.b@mail.ru>
2024-02-11 19:39:41 +0300

    Спринт 3, приложение счетчик

--- bookCounter/ViewController.swift
+++ bookCounter/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var infoCount: UILabel!
+    @IBOutlet weak var logTextView: UITextView!
+   
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        logTextView.text = "История изменений:\n"
     }
-
-
+    
+    @IBAction func dereaseAction() {
+        guard count > 0 else { return }
+        count -= 1
+        updateCounter()
+        logAction(action: "значение изменено на -1")
+    }
+    
+    @IBAction func increaseAction() {
+        count += 1
+        updateCounter()
+        logAction(action: "значение изменено на +1")
+    }
+    
+    @IBAction func clearAction() {
+        count = 0
+        updateCounter()
+        logAction(action: "значение сброшено")
+    }
+    
+    @IBAction func tapButton() {
+        count += 1
+        updateCounter()
+    }
+    
+    func updateCounter() {
+        infoCount.text = "Значение счетчика: \(count)"
+    }
+    
+    func logAction(action: String) {
+        let line = "\(Date()): \(action)\n"
+        logTextView.text += line
+    }
 }
-
